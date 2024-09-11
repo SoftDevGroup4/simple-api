2727a3408779f33a033179682d76f047de24a136
BaitoeyKP <kanyarat.phookosot@gmail.com>
2024-09-11 13:44:59 +0700

    update plus display format

--- app.py
+++ app.py
@@ -19,11 +19,11 @@ def calculate(num1, num2):
         num1 = eval(num1)
         num2 = int(num2)
 
-        results = {"plus": num1 + num2}
+        results = f"{num1} + {num2} = {num1 + num2}"
     except:
         results = {"error_msg": "inputs must be numbers"}
 
-    return jsonify(results)
+    return results
 
 
 if __name__ == "__main__":
