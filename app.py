1adf8254408e933196c84a06d5f05407df456c06
nattapon.NUT <88678470+64010248Nattapon@users.noreply.github.com>
2024-09-18 15:31:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Index!nutt555updateNewnewwhello"
+    return "Index!nutt555updateNewnewwhelloWorld"
 
 
 @app.route("/getcode", methods=["GET"])
