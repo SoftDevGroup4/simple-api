5144612de8853b5ff6468079ccd99532b55c7a03
BaitoeyKP <kanyarat.phookosot@gmail.com>
2024-09-30 17:04:04 +0700

    .

--- test_unit.py
+++ test_unit.py
@@ -1,24 +1,24 @@
 import unittest
-import app
+from app import app
 
 
 class TestApp(unittest.TestCase):
-
+    
     def true_when_x_is_17(self):
-        response = self.app.get("/is_prime/17")
+        response = app.get("/is_prime/17")
         self.assertEqual(response.data.decode(), "True")
 
     def false_when_x_is_36(self):
-        response = self.app.get("/is_prime/36")
-        self.assertEqual(response.data.decode(), "False")
+        response = app.get("/is_prime/36")
+        self.assertEqual(response.data.decode(), "True")
 
     def true_when_x_is_13219(self):
-        response = self.app.get("/is_prime/13219")
+        response = app.get("/is_prime/13219")
         self.assertEqual(response.data.decode(), "True")
 
     def true_when_x_is_17(self):
         # Test for invalid input
-        response = self.app.get("/is_prime/a")
+        response = app.get("/is_prime/a")
         self.assertEqual(response.json, {"error_msg": "inputs must be numbers"})
 
 
