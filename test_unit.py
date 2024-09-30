9f98b224171e7d01a9e59ccb2524766c1913b6c6
BaitoeyKP <kanyarat.phookosot@gmail.com>
2024-09-30 17:07:53 +0700

    Update test_unit.py

--- test_unit.py
+++ test_unit.py
@@ -5,22 +5,22 @@ from app import app
 class TestApp(unittest.TestCase):
     
     def true_when_x_is_17(self):
-        response = app.get("/is_prime/17")
+        response = self.app.get("/is_prime/17")
         self.assertEqual(response.data.decode(), "True")
 
     def false_when_x_is_36(self):
-        response = app.get("/is_prime/36")
-        self.assertEqual(response.data.decode(), "True")
+        response = self.app.get("/is_prime/36")
+        self.assertEqual(response.data.decode(), "False")
 
     def true_when_x_is_13219(self):
-        response = app.get("/is_prime/13219")
+        response = self.app.get("/is_prime/13219")
         self.assertEqual(response.data.decode(), "True")
 
-    def true_when_x_is_17(self):
+    def invalid_input(self):
         # Test for invalid input
-        response = app.get("/is_prime/a")
+        response = self.app.get("/is_prime/a")
         self.assertEqual(response.json, {"error_msg": "inputs must be numbers"})
 
 
-if __name__ == "__main__a":
+if __name__ == "__main__":
     unittest.main()
