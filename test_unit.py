d3ee812f357dc05848e03a9c6917219fad95a045
BaitoeyKP <kanyarat.phookosot@gmail.com>
2024-10-01 18:21:27 +0700

    create is_even api

--- test_unit.py
+++ test_unit.py
@@ -5,16 +5,16 @@ import app
 class TestApp(unittest.TestCase):
 
     def test_true_when_x_is_17(self):
-        response = app.is_prime(17)
-        self.assertEqual(response, "True")
-
-    def test_false_when_x_is_36(self):
-        response = app.is_prime(36)
+        response = app.is_even(17)
         self.assertEqual(response, "False")
 
-    def test_true_when_x_is_13219(self):
-        response = app.is_prime(13219)
+    def test_false_when_x_is_36(self):
+        response = app.is_even(36)
         self.assertEqual(response, "True")
+
+    def test_true_when_x_is_13219(self):
+        response = app.is_even(13219)
+        self.assertEqual(response, "False")
 
 if __name__ == "__main__":
     unittest.main()
