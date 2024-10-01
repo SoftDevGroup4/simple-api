bacf55071b7ad8e6013082c3da545a7f86d77bb2
BaitoeyKP <kanyarat.phookosot@gmail.com>
2024-10-01 18:57:18 +0700

    commit API grade

--- test_unit.py
+++ test_unit.py
@@ -5,16 +5,16 @@ import app
 class TestApp(unittest.TestCase):
 
     def test_true_when_x_is_17(self):
-        response = app.is_even(17)
-        self.assertEqual(response, "False")
+        response = app.is_grade(3.7)
+        self.assertEqual(response, "1st")
 
     def test_false_when_x_is_36(self):
-        response = app.is_even(36)
-        self.assertEqual(response, "True")
+        response = app.is_grade(3.3)
+        self.assertEqual(response, "2nd")
 
     def test_true_when_x_is_13219(self):
-        response = app.is_even(13219)
-        self.assertEqual(response, "False")
+        response = app.is_grade(2.5)
+        self.assertEqual(response, "other")
 
 if __name__ == "__main__":
     unittest.main()
